5de7653973a1cf5419c76dbecd1e9ba44725119e
Anotinas <daviddaviddele@gmail.com>
2019-09-18 13:14:13 +0200

    Running android  pipeline

--- Android/Tests/preliminaryTest.java
+++ Android/Tests/preliminaryTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 public class TestingSetup{
     
     @Test
-    public void multiplicationOfZeroIntegersShouldReturnZero() {
-        MyClass tester = new MyClass(); // MyClass is tested
+    public void PreminTest() {
 
         // assert statements
-        assertEquals(0, tester.multiply(10, 0), "10 x 0 must be 0");
-        assertEquals(0, tester.multiply(0, 10), "0 x 10 must be 0");
-        assertEquals(0, tester.multiply(0, 0), "0 x 0 must be 0");
+        assertEquals(0, 10*0, "10 x 0 must be 0");
+        assertFalse(0==3);
     }
 }
