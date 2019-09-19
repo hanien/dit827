c4d2afa850d4ba3cd9856ab2c2f215be2034636e
Anotinas <daviddaviddele@gmail.com>
2019-09-19 12:23:10 +0200

    Changing android environment

--- Android/app/src/androidTest/java/com/preliminaryTest.java
+++ Android/app/src/androidTest/java/com/preliminaryTest.java
@@ -7,8 +7,9 @@ public class TestingSetup{
     @Test
     public void PreminTest() {
 
+        int x = 3;
         // assert statements
-        assertEquals(0, 10*0, "10 x 0 must be 0");
-        assertFalse(0==3);
+        assertEquals(0, x*0, "10 x 0 must be 0");
+        assertEquals((0==x)==false);
     }
 }
