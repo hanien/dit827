39421a7701d14bc8ad47950cbc841ee89d054bdd
Anotinas <daviddaviddele@gmail.com>
2019-11-03 21:46:31 +0100

    testing a test

--- Pi/Tests/test_Sensor_Reader.py
+++ Pi/Tests/test_Sensor_Reader.py
@@ -9,7 +9,7 @@ seat = "test"
 
 def test_resetTestDatabase():
     req = {
-    "temperature": "0",
+    "temperature": "1",
     "humidity": "0",
     "pressure": "0",
     "altitude": "0",
@@ -21,13 +21,15 @@ def test_resetTestDatabase():
     r = requests.put("http://dit827aptiv.herokuapp.com/api/sensors/"+ seat, data=json.dumps(req))
 
 
-def test_calling_Sensor():
+def test_nonBoard_Stores_data():
     nonBoard.temperature_total = 1
-    assert nonBoard.temperature_total == 1
+    nonBoard.temperature_total += 3
+    assert nonBoard.temperature_total == 4
 
 def test_sendingData():
     nonBoard.seat = seat
     test_not_over = True
+
 
 
 
