433a0e3a9d514b001026f270a99bc407dde2828d
Anotinas <daviddaviddele@gmail.com>
2019-11-03 22:02:49 +0100

    worked on the loop test

--- Pi/Tests/test_Sensor_Reader.py
+++ Pi/Tests/test_Sensor_Reader.py
@@ -7,9 +7,9 @@ import json
 
 seat = "test"
 
-def test_resetTestDatabase():
+def resetTestDatabase():
     req = {
-    "temperature": "1",
+    "temperature": "0",
     "humidity": "0",
     "pressure": "0",
     "altitude": "0",
@@ -18,7 +18,7 @@ def test_resetTestDatabase():
     "ir": "0",
     "full": "0"
     }
-    r = requests.post("http://dit827aptiv.herokuapp.com/api/sensors/"+ seat, data=json.dumps(req))
+    r = requests.put("http://dit827aptiv.herokuapp.com/api/sensors/"+ seat, data=json.dumps(req))
 
 
 def test_nonBoard_Stores_data():
@@ -27,8 +27,23 @@ def test_nonBoard_Stores_data():
     assert nonBoard.temperature_total == 4
 
 def test_sendingData():
+    resetTestDatabase()
     nonBoard.seat = seat
     test_not_over = True
+
+    nonBoard.average_and_send()
+
+    #The test should fail if any the values on the website are not 30
+    while(test_not_over):
+        nonBoard.full_total += 30
+        nonBoard.ir_total += 30
+        nonBoard.lux_total += 30
+        nonBoard.gain_total += 30
+        nonBoard.temperature_total += 30
+        nonBoard.humidity_total += 30
+        nonBoard.pressure_total += 30
+        nonBoard.altitude_total += 30
+        nonBoard.counter += 1
 
 
 
