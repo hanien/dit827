f268a71fe8fa32e9707f0c97e1afc008b15e3314
Anotinas <daviddaviddele@gmail.com>
2019-12-11 10:23:47 +0100

    Fixed unittests since the methods they were called were changed

--- Android/app/src/test/java/com/example/aptiv/Model/Helper/ProfileHelperTest.java
+++ Android/app/src/test/java/com/example/aptiv/Model/Helper/ProfileHelperTest.java
@@ -28,59 +28,59 @@ public class ProfileHelperTest {
     @Test
     public void TempDifference_Correct()
     {
-        assertTrue(ProfileHelper.checkTemp(targetSuccess, comparison1, comparison2));
+        assertTrue(ProfileHelper.checkTemp(false,targetSuccess, comparison1, comparison2));
     }
 
     @Test
     public void SoundDifference_Correct()
     {
-        assertTrue(ProfileHelper.checkSound(targetSuccess, comparison1, comparison2));
+        assertTrue(ProfileHelper.checkSound(false,targetSuccess, comparison1, comparison2));
     }
 
     @Test
     public void HumidityDifference_Correct()
     {
-        assertTrue(ProfileHelper.checkHumidity(targetSuccess, comparison1, comparison2));
+        assertTrue(ProfileHelper.checkHumidity(false,targetSuccess, comparison1, comparison2));
     }
 
     @Test
     public void PressureDifference_Correct()
     {
-        assertTrue(ProfileHelper.checkAirPressure(targetSuccess, comparison1, comparison2));
+        assertTrue(ProfileHelper.checkAirPressure(false,targetSuccess, comparison1, comparison2));
     }
 
     @Test
     public void LightDifference_Correct()
     {
-        assertTrue(ProfileHelper.checkLux(targetSuccess, comparison1, comparison2));
+        assertTrue(ProfileHelper.checkLux(false,targetSuccess, comparison1, comparison2));
     }
     private Zone targetFailSmall = new Zone(Zone.ZoneName.DRIVER, "17.0", "44.0", "0.0", "60.0", "0.0", "24.0", "894.0", "24.0", "0.0", "0.0", "21.0");
 
     @Test
     public void TempDifference_Fail_Small()
     {
-        assertFalse(ProfileHelper.checkTemp(targetFailSmall, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkTemp(false,targetFailSmall, comparison1, comparison2));
     }
 
     @Test
     public void SoundDifference_Fail_Small()
     {
-        assertFalse(ProfileHelper.checkSound(targetFailSmall, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkSound(false,targetFailSmall, comparison1, comparison2));
     }
     @Test
     public void HumidityDifference_Fail_Small()
     {
-        assertFalse(ProfileHelper.checkHumidity(targetFailSmall, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkHumidity(false,targetFailSmall, comparison1, comparison2));
     }
     @Test
     public void PressureDifference_Fail_Small()
     {
-        assertFalse(ProfileHelper.checkAirPressure(targetFailSmall, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkAirPressure(false,targetFailSmall, comparison1, comparison2));
     }
     @Test
     public void LightDifference_Fail_Small()
     {
-        assertFalse(ProfileHelper.checkLux(targetFailSmall, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkLux(false,targetFailSmall, comparison1, comparison2));
     }
 
     private Zone targetFailMed = new Zone(Zone.ZoneName.DRIVER, "14.0", "38.0", "0.0", "60.0", "0.0", "56.0", "882.0", "12.0", "0.0", "0.0", "21.0");
@@ -88,28 +88,28 @@ public class ProfileHelperTest {
     @Test
     public void TempDifference_Fail_Med()
     {
-        assertFalse(ProfileHelper.checkTemp(targetFailMed, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkTemp(false,targetFailMed, comparison1, comparison2));
     }
 
     @Test
     public void SoundDifference_Fail_Med()
     {
-        assertFalse(ProfileHelper.checkSound(targetFailMed, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkSound(false,targetFailMed, comparison1, comparison2));
     }
     @Test
     public void HumidityDifference_Fail_Med()
     {
-        assertFalse(ProfileHelper.checkHumidity(targetFailMed, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkHumidity(false,targetFailMed, comparison1, comparison2));
     }
     @Test
     public void PressureDifference_Fail_Med()
     {
-        assertFalse(ProfileHelper.checkAirPressure(targetFailMed, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkAirPressure(false,targetFailMed, comparison1, comparison2));
     }
     @Test
     public void LightDifference_Fail_Med()
     {
-        assertFalse(ProfileHelper.checkLux(targetFailMed, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkLux(true,targetFailMed, comparison1, comparison2));
     }
 
     private Zone targetFailLarge = new Zone(Zone.ZoneName.DRIVER, "11.0", "32.0", "0.0", "60.0", "0.0", "0.0", "888.0", "18.0", "0.0", "0.0", "21.0");
@@ -117,28 +117,28 @@ public class ProfileHelperTest {
     @Test
     public void TempDifference_Fail_Large()
     {
-        assertFalse(ProfileHelper.checkTemp(targetFailLarge, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkTemp(false,targetFailLarge, comparison1, comparison2));
     }
 
     @Test
     public void SoundDifference_Fail_Large()
     {
-        assertFalse(ProfileHelper.checkSound(targetFailLarge, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkSound(false,targetFailLarge, comparison1, comparison2));
     }
     @Test
     public void HumidityDifference_Fail_Large()
     {
-        assertFalse(ProfileHelper.checkHumidity(targetFailLarge, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkHumidity(false,targetFailLarge, comparison1, comparison2));
     }
     @Test
     public void PressureDifference_Fail_Large()
     {
-        assertFalse(ProfileHelper.checkAirPressure(targetFailLarge, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkAirPressure(false,targetFailLarge, comparison1, comparison2));
     }
     @Test
     public void LightDifference_Fail_Large()
     {
-        assertFalse(ProfileHelper.checkLux(targetFailLarge, comparison1, comparison2));
+        assertFalse(ProfileHelper.checkLux(false,targetFailLarge, comparison1, comparison2));
     }
 
     //two cases : success and fail
